212fabda7ad3e688cabfba48ec1f577be7cafea1
Vexatos <Stuarzt@gmx.de>
2015-06-14 14:20:10 +0200

    Getting started

---
+++ src/main/java/vexatos/factumopus/FactumOpus.java
@@ -0,0 +1,35 @@
+package vexatos.factumopus;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import vexatos.factumopus.reference.Mods;
+
+/**
+ * @author Vexatos
+ */
+@Mod(modid = Mods.FactumOpus, name = Mods.FactumOpus_NAME, version = "@VERSION@",
+	dependencies = "required-after:" + Mods.Factorization)
+public class FactumOpus {
+
+	@Instance(Mods.FactumOpus)
+	public static FactumOpus instance;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent e) {
+
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e) {
+
+	}
+}
---
+++ src/main/java/vexatos/factumopus/reference/Mods.java
@@ -0,0 +1,15 @@
+package vexatos.factumopus.reference;
+
+/**
+ * @author Vexatos
+ */
+public class Mods {
+	public static final String
+		FactumOpus = "FactumOpus",
+		FactumOpus_NAME = "Factum Opus";
+
+	public static final String
+		Botany = "Botany",
+		Factorization = "factorization",
+		Forestry = "Forestry";
+}
